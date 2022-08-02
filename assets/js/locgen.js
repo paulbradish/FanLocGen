6eef91341c24de00cedfbdbcf4503f7af7cd954e
Paul Bradish <pbradish@gmail.com>
2022-08-02 00:19:23 -0500

    Add files via upload

---
+++ assets/js/locgen.js
@@ -0,0 +1,163 @@
+// Location Generator //
+
+var adj1 = [
+"", 
+"Abandoned", 
+"Ancient", 
+"Cold", 
+"Curious", 
+"Cursed", 
+"Decaying", 
+"Desolate", 
+"Dreadful",
+"Enchanted",
+"Fallen", 
+"Forbidden", 
+"Forgotten", 
+"Frightening",
+"Frigid", 
+"Grim",
+"Haunted", 
+"Hot", 
+"Humid", 
+"Isolated", 
+"Lavish",
+"Lonely",
+"Old", 
+"Magic", 
+"Magical", 
+"Mighty", 
+"New", 
+"Petrified", 
+"Rancid",
+"Rough",
+"Steamy", 
+"Terrifying",
+"Uncanny",
+"Underground", 
+"Warm"
+] 
+var obj1 = [
+"Bridge",
+"Canyon",
+"Catacombs", 
+"Cave", 
+"Cavern", 
+"Chamber", 
+"City", 
+"Coast", 
+"Crypt", 
+"Mountain", 
+"Swamp", 
+"Marsh", 
+"Tar Pit", 
+"Peat Bog", 
+"River", 
+"Ocean",
+"Lair", 
+"Lake", 
+"Pond", 
+"Stream", 
+"Forest", 
+"Taiga", 
+"Tundra", 
+"Grassland", 
+"Valley",
+"Hall", 
+"Halls", 
+"Ruins", 
+"Sewer", 
+"Town", 
+"Toll Bridge",
+"Village", 
+"Shore", 
+"Outcropping", 
+"Fissure", 
+"Realm", 
+"Kingdom", 
+"Desert", 
+"Tower", 
+"Castle", 
+"Shoreline", 
+"Jungle", 
+"Pit", 
+"Savannah",
+"Sepulcher",
+"Shrine", 
+"Road", 
+"Island",
+"Vault", 
+"Volcano",
+"Wasteland"
+]
+var adj2 = [
+"", 
+"of the Ages",
+"of the Agnostic",
+"of the Bear",
+"of Blood",
+"of the Cloth", 
+"of the Cross", 
+"of the Dead", 
+"of Death",
+"of Demons",
+"of the Defiant", 
+"of the Holy",
+"of Solitude", 
+"of Secrets", 
+"of Mystery", 
+"of the Order", 
+"of Pain", 
+"of Longing", 
+"of Prayer", 
+"of Whispers", 
+"of Pestilence", 
+"of Lies", 
+"of the Wolf",
+"of the Gods", 
+"of Stone", 
+"of Rock", 
+"of Life", 
+"of Truth"
+] 
+var list;
+
+    function generator() {
+    document.getElementById("name1").innerHTML = adj1[Math.floor(Math.random() * adj1.length)] + " " + obj1[Math.floor(Math.random() * obj1.length)] + " " + adj2[Math.floor(Math.random() * adj2.length)];;;
+    }
+
+    function generator2() {
+    document.getElementById("name2").innerHTML = adj1[Math.floor(Math.random() * adj1.length)] + " " + obj1[Math.floor(Math.random() * obj1.length)] + " " + adj2[Math.floor(Math.random() * adj2.length)];;;
+    }
+
+    function generator3() {
+    document.getElementById("name3").innerHTML = adj1[Math.floor(Math.random() * adj1.length)] + " " + obj1[Math.floor(Math.random() * obj1.length)] + " " + adj2[Math.floor(Math.random() * adj2.length)];;;
+    }
+
+    function generator4() {
+    document.getElementById("name4").innerHTML = adj1[Math.floor(Math.random() * adj1.length)] + " " + obj1[Math.floor(Math.random() * obj1.length)] + " " + adj2[Math.floor(Math.random() * adj2.length)];;;
+    }
+
+    function generator5() {
+    document.getElementById("name5").innerHTML = adj1[Math.floor(Math.random() * adj1.length)] + " " + obj1[Math.floor(Math.random() * obj1.length)] + " " + adj2[Math.floor(Math.random() * adj2.length)];;;
+    }
+
+    function generator6() {
+    document.getElementById("name6").innerHTML = adj1[Math.floor(Math.random() * adj1.length)] + " " + obj1[Math.floor(Math.random() * obj1.length)] + " " + adj2[Math.floor(Math.random() * adj2.length)];;;
+    }
+
+    function generator7() {
+    document.getElementById("name7").innerHTML = adj1[Math.floor(Math.random() * adj1.length)] + " " + obj1[Math.floor(Math.random() * obj1.length)] + " " + adj2[Math.floor(Math.random() * adj2.length)];;;
+    }
+
+    function generator8() {
+    document.getElementById("name8").innerHTML = adj1[Math.floor(Math.random() * adj1.length)] + " " + obj1[Math.floor(Math.random() * obj1.length)] + " " + adj2[Math.floor(Math.random() * adj2.length)];;;
+    }   
+
+    function generator9() {
+    document.getElementById("name9").innerHTML = adj1[Math.floor(Math.random() * adj1.length)] + " " + obj1[Math.floor(Math.random() * obj1.length)] + " " + adj2[Math.floor(Math.random() * adj2.length)];;;
+    }  
+
+    function generator10() {
+    document.getElementById("name10").innerHTML = adj1[Math.floor(Math.random() * adj1.length)] + " " + obj1[Math.floor(Math.random() * obj1.length)] + " " + adj2[Math.floor(Math.random() * adj2.length)];;;
+    }  
